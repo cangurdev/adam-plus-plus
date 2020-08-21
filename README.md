2321badccab459cf8be6f957e3c2ce9ac04278c5
Can <cangur2398@gmail.com>
2020-08-22 01:53:18 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# adam-plus-plus
+
+Adam gibi değil, adam++ bir Discord botu.
+
+## Komutlar
+| Komut | Açıklama | 
+| - | - |
+| +adamm @kullaniciadi | Etiketlenen kullanıcının adamlığını över |
+| +cuma | Cumanızı kutlar |
+| +yüzde | Bilimsel hesaplamalar yaparak adamlık yüzdenizi söyler |
+
+## Özellikler
+- 31 adet adamlık sözü
+- 9 adet cuma mesajı
---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const words = require('./adam.json');
+const cuma = require('./cuma.json');
+const token = require('./config.json');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    try{
+        if (msg.content.startsWith("+adamm")) {
+            let word = words[Math.floor(Math.random() * 30)];
+            const taggedUser = msg.mentions.users.first();
+            msg.channel.send(`<@${taggedUser.id}>`+" "+word);
+        }
+        else if (msg.content === "+cuma"){
+            let cumaMessage = cuma[Math.floor(Math.random() * 8)];
+            msg.channel.send(""+cumaMessage);
+        }
+        else if (msg.content === "+yüzde"){
+            let percentage = Math.floor(Math.random()*100)
+            msg.reply("Hesaplamalarıma göre %"+percentage+" adamsın");
+        }
+    }
+    catch(error){
+        msg.channel.send("Buralarda bir adam göremedim.")
+    }
+});
+
+client.login(token);
