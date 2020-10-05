5a8b2d66894e58ff67fafd1994a4639bff66a9b9
Can <cangur2398@gmail.com>
2020-10-05 17:15:32 +0300

    fixed login issue

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const Discord = require('discord.js');
-const words = require('./adam.json');
-const cuma = require('./cuma.json');
-const token = require('./config.json');
+const Discord = require("discord.js");
+const words = require("./adam.json");
+const cuma = require("./cuma.json");
+const token = require("./config.json");
 
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    try{
-        if (msg.content.startsWith("+adamm")) {
-            let word = words[Math.floor(Math.random() * 31)];
-            const taggedUser = msg.mentions.users.first();
-            msg.channel.send(`<@${taggedUser.id}>`+" "+word);
-        }
-        else if (msg.content === "+cuma"){
-            let cumaMessage = cuma[Math.floor(Math.random() * 11)];
-            msg.channel.send(""+cumaMessage);
-        }
-        else if (msg.content === "+yüzde"){
-            let percentage = Math.floor(Math.random()*100)
-            msg.reply("Hesaplamalarıma göre %"+percentage+" adamsın");
-        }
+client.on("message", (msg) => {
+  try {
+    if (msg.content.startsWith("+adamm")) {
+      let word = words[Math.floor(Math.random() * 31)];
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`<@${taggedUser.id}>` + " " + word);
+    } else if (msg.content === "+cuma") {
+      let cumaMessage = cuma[Math.floor(Math.random() * 11)];
+      msg.channel.send("" + cumaMessage);
+    } else if (msg.content === "+yüzde") {
+      let percentage = Math.floor(Math.random() * 100);
+      msg.reply("Hesaplamalarıma göre %" + percentage + " adamsın");
     }
-    catch(error){
-        msg.channel.send("Buralarda bir adam göremedim.")
-    }
+  } catch (error) {
+    msg.channel.send("Buralarda bir adam göremedim.");
+  }
 });
 
-client.login(token);
+client.login(token.token);
