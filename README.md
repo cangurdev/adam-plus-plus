36d9dbc0e22fb96b05ef985516334b3f3b5d420f
Can <cangur2398@gmail.com>
2020-10-05 22:53:19 +0300

    added ısmarla & nargile commands

--- README.md
+++ README.md
@@ -3,12 +3,17 @@
 Adam gibi değil, adam++ bir Discord botu.
 
 ## Komutlar
-| Komut | Açıklama | 
-| - | - |
-| +adamm @kullaniciadi | Etiketlenen kullanıcının adamlığını över |
-| +cuma | Cumanızı kutlar |
-| +yüzde | Bilimsel hesaplamalar yaparak adamlık yüzdenizi söyler |
+
+| Komut                  | Açıklama                                               |
+| ---------------------- | ------------------------------------------------------ |
+| +adamm @kullaniciadi   | Etiketlenen kullanıcının adamlığını över               |
+| +cuma                  | Cumanızı kutlar                                        |
+| +yüzde                 | Bilimsel hesaplamalar yaparak adamlık yüzdenizi söyler |
+| +nargile               | Bugün hangi aromalı nargile içeceğinizi belirler       |
+| +ısmarla @kullaniciadi | Etkilenen kullanıcının adamlığına bir nargile ısmarlar |
 
 ## Özellikler
+
 - 32 adet adamlık sözü
 - 12 adet cuma mesajı
+- 24 adet nargile çeşidi
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 const words = require("./adam.json");
 const cuma = require("./cuma.json");
-const token = require("./config.json");
+const nargile = require("./nargile.json");
+const config = require("./config.json");
 
 const client = new Discord.Client();
 
@@ -14,17 +15,26 @@ client.on("message", (msg) => {
     if (msg.content.startsWith("+adamm")) {
       let word = words[Math.floor(Math.random() * 31)];
       const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`<@${taggedUser.id}>` + " " + word);
+      msg.channel.send(`<@${taggedUser.id}> ${word}`);
     } else if (msg.content === "+cuma") {
       let cumaMessage = cuma[Math.floor(Math.random() * 11)];
       msg.channel.send("" + cumaMessage);
     } else if (msg.content === "+yüzde") {
       let percentage = Math.floor(Math.random() * 100);
-      msg.reply("Hesaplamalarıma göre %" + percentage + " adamsın");
+      msg.reply(`Hesaplamalarıma göre %${percentage} adamsın`);
+    } else if (msg.content.startsWith("+ısmarla")) {
+      let hookah = nargile[Math.floor(Math.random() * 24)];
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(
+        `<@${taggedUser.id}> adamlığına bir ${hookah} aromalı nargile söylüyorum `
+      );
+    } else if (msg.content === "+nargile") {
+      let hookah = nargile[Math.floor(Math.random() * 24)];
+      msg.reply(`Bugün ${hookah} aromalı nargile içmelisin `);
     }
   } catch (error) {
     msg.channel.send("Buralarda bir adam göremedim.");
   }
 });
 
-client.login(token.token);
+client.login(config.token);
