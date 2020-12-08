cb253ac6f7f5cec3b1f7ef2327e9f3eccf1822ac
Can Gür <cangur2398@gmail.com>
2020-12-08 17:55:16 +0300

    created content folder & '+öv' command

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
-const words = require("./adam.json");
-const fridayMessages = require("./cuma.json");
-const hookahs = require("./nargile.json");
-
+const words = require("./contents/adam.json");
+const fridayMessages = require("./contents/cuma.json");
+const hookahs = require("./contents/nargile.json");
+const ovguler = require("./contents/ovgu.json");
 const client = new Discord.Client();
 
 client.on("ready", () => {
@@ -15,15 +15,18 @@ client.on("message", (msg) => {
       let word = words[Math.floor(Math.random() * Object.keys(words).length)];
       const taggedUser = msg.mentions.users.first();
       msg.channel.send(`<@${taggedUser.id}> ${word}`);
+
     } else if (msg.content === "+cuma") {
       let fridayMessage =
         fridayMessages[
         Math.floor(Math.random() * Object.keys(fridayMessages).length)
         ];
       msg.channel.send("" + fridayMessage);
+
     } else if (msg.content === "+yüzde") {
       let percentage = Math.floor(Math.random() * 100);
       msg.reply(`Hesaplamalarıma göre %${percentage} adamsın`);
+
     } else if (msg.content.startsWith("+ısmarla")) {
       let hookah =
         hookahs[Math.floor(Math.random() * Object.keys(hookahs).length)];
@@ -31,14 +34,21 @@ client.on("message", (msg) => {
       msg.channel.send(
         `<@${taggedUser.id}> adamlığına bir ${hookah} aromalı nargile söylüyorum `
       );
+
     } else if (msg.content === "+nargile") {
       let aroma =
         hookahs[Math.floor(Math.random() * Object.keys(hookahs).length)];
       msg.reply(`Bugün ${aroma} aromalı nargile içmelisin `);
+
     } else if (msg.content === "+dolar") {
       msg.reply("Dolarla mı maaş alıyorsun?")
-    }
-    else if (msg.content === "+yardım") {
+
+    } else if (msg.content.startsWith("+öv")) {
+      let ovgu = ovguler[Math.floor(Math.random() * Object.keys(ovguler).length)];
+      const taggedUser = msg.mentions.users.first();
+      msg.channel.send(`<@${taggedUser.id}> ${ovgu}`);
+
+    } else if (msg.content === "+yardım") {
       const help = new Discord.MessageEmbed()
         .setTitle("Komutlar")
         .setColor(0xff0000).setDescription(`
@@ -52,7 +62,10 @@ client.on("message", (msg) => {
       
        **+ısmarla @kullaniciadi** - Etiketlenen kullanıcının adamlığına bir nargile ısmarlar 
        
-       **+dolar** - Güncel dolar kurunu gösterir `);
+       **+dolar** - Güncel dolar kurunu gösterir
+       
+       **+öv @kullaniciadi** - Etiketlenen kullanıcıyı över
+       `);
       msg.reply(help);
     }
   } catch (error) {
